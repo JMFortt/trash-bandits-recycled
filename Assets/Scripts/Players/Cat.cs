c468faba08c349a143a23220d70a11b06ef18b00
blurble birb <ashhong03@gmail.com>
2024-12-05 15:17:19 -0500

    cat cooldown indicator, score text fix

--- Assets/Scripts/Players/Cat.cs
+++ Assets/Scripts/Players/Cat.cs
@@ -1,3 +1,4 @@
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,14 +8,15 @@ public class Cat : MonoBehaviour
     [SerializeField] public int caught = 0;
     [SerializeField] public float hold_time = 0, cooldown = 0,speed;
     [SerializeField] public NavMeshAgent human;
-    [SerializeField] public SpriteRenderer cat_sprite;
-    [SerializeField] public Sprite cat_left, cat_right;
+    [SerializeField] public SpriteRenderer cat_sprite, symbol_sprite;
+    [SerializeField] public Sprite cat_left, cat_right, distract_sprite, ready_sprite;
     private bool holding = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         speed = human.speed;
+        //symbol_sprite.sprite = null;
     }
 
     // Update is called once per frame
@@ -29,10 +31,13 @@ public class Cat : MonoBehaviour
         {
             cooldown += Time.deltaTime;
         }
+        if (cooldown > 5) symbol_sprite.sprite = ready_sprite;
+        else if (cooldown <= 5) symbol_sprite.sprite = null;
 
         // cancel ability early (lift key)
         if ((Input.GetKeyUp(KeyCode.RightShift)) && holding)
         {
+            symbol_sprite.sprite = null;
             GameObject.FindGameObjectWithTag("manager").GetComponent<AudioManager>().Stop("cat_meow");
             holding = false;
             human.speed = speed;
@@ -58,6 +63,7 @@ public class Cat : MonoBehaviour
             {
                 if (cooldown >= 5)
                 {
+                    symbol_sprite.sprite = distract_sprite;
                     holding = true;
                     human.speed = 0;
                     hold_time = 0;
@@ -74,6 +80,7 @@ public class Cat : MonoBehaviour
                 // ability time up (end)
                 else
                 {
+                    symbol_sprite.sprite = null;
                     holding = false;
                     human.speed = speed;
                     GameObject.FindGameObjectWithTag("manager").GetComponent<AudioManager>().Stop("cat_meow");
@@ -88,6 +95,7 @@ public class Cat : MonoBehaviour
         // cancel ability early (leave LoS)
         if ((collider.GetComponent<Collider>() == human_collider) && holding)
         {
+            symbol_sprite.sprite = null;
             holding = false;
             human.speed = speed;
             cooldown = 0;
